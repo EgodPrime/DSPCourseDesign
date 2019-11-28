a2ce2e82835aa176c621cd7bdb8c2d548a3ef48b
egod <singlebone@foxmail.com>
2019-11-28 21:39:50 +0800

    添加8进制的小数与二进制数组转换

--- 基于DCT的隐藏算法/代码/audio2bin2.m
+++ 基于DCT的隐藏算法/代码/audio2bin2.m
@@ -1,4 +1,18 @@
+% 将音频信号转换为带符号的8位二进制信号
+% 输入：1声道音频（推荐）或者2声道音频
+%       形状：(m,1)或(m,2)
+% 输出：二维二进制矩阵
+%       形状：(m,8)
 function bin = audio2bin2(au)
+% 合并双声道
+    if min(size(au))==2
+        au = mean(au,2);
+    end
     gau = gpuArray(au);
-    
+    bin = zeros(length(au),8);
+    bin = gpuArray(bin);
+    for i = 1:length(au)
+        bin(i,:) = float2bin2(gau(i));
+    end
+    bin = gather(bin);
 end
--- 基于DCT的隐藏算法/代码/float2bin2.m
+++ 基于DCT的隐藏算法/代码/float2bin2.m
@@ -1,8 +1,23 @@
+% È·±£±»×ª»»µÄĞ¡Êı½éÓÚ-1ÖÁ1Ö®¼ä£¨²»°üº¬£©
+
 function bin = float2bin2(num)
+    bin = zeros(1,8);
     base = 0.5;
-    if num >0
-        bin(1)=
-    for i = 2:8
-        
+    
+    if num > 0
+        bin(1)=0;
+    else
+        bin(1)=1;
     end
+    
+    for i = 2:8
+        if num > base
+            bin(i) = 1;
+            num = num - base;
+        else
+            bin(i) = 0;
+        end
+        base = base/2;
+    end
+    
 end
