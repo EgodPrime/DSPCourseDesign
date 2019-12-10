8b640b2fde5d47398c764db94343f85133383267
eve <2996188148@qq.com>
2019-12-10 17:35:33 +0800

    eve

--- 回声隐藏算法/代码/check.m
+++ 回声隐藏算法/代码/check.m
@@ -1,5 +1,6 @@
 function check(music)
- capacity = length(music)/2000/4*8
- dispf("可以隐藏%d")
-
+ capacity = length(music)/2000/4*8;
+ sprintf("可以隐藏%d比特文件\n",capacity);
+ disp("按任意键继续……");
+ pause();
 end
